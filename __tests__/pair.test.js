1ed6547927edc0612bdba28e4098e3b6b7aee1ac
vikzh <vikt.anmail@gmail.com>
2019-03-14 18:14:34 +0300

    tests

---
+++ __tests__/pair.test.js
@@ -0,0 +1,9 @@
+import { cons, car, cdr, toString } from "../src";
+
+describe('Pair', () => {
+  it('#cons', () => {
+    const pair = cons(1, 2);
+    expect(car(pair)).toBe(1);
+    expect(cdr(pair)).toBe(2);
+  });
+});
