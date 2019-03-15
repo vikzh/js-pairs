c3ca39ae532c4897dc3cb91a8ce89df4757f6719
vikzh <vikt.anmail@gmail.com>
2019-03-15 12:01:35 +0300

    tests added

--- __tests__/pair.test.js
+++ __tests__/pair.test.js
@@ -8,8 +8,32 @@ describe('Pair', () => {
     expect(car(pair)).toBe(1);
     expect(cdr(pair)).toBe(2);
   });
+
   it('#toString', () => {
     const pair = cons('text1', 'text2');
     expect(toString(pair)).toBe('(text1, text2)');
+  });
+
+  it('#toString2', () => {
+    const pair = cons(5, cons(7, 9));
+    expect(toString(pair)).toBe('(5, (7, 9))');
+  });
+
+  it('#checkPair', () => {
+    expect(() => {
+      car('test');
+    }).toThrowErrorMatchingSnapshot();
+
+    expect(() => {
+      cdr(2345);
+    }).toThrowErrorMatchingSnapshot();
+
+    expect(() => {
+      car(() => 1);
+    }).toThrowErrorMatchingSnapshot();
+
+    expect(() => {
+      car({ key: 'testValue' });
+    }).toThrowErrorMatchingSnapshot();
   });
 });
